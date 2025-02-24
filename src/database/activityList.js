7a427f42a304e449037002493a4cd579882f5090
Danto <rlarladlstjd@gmail.com>
2025-02-24 16:36:02 +0900

    master

--- src/database/activityList.js
+++ src/database/activityList.js
@@ -242,58 +242,74 @@ export const ACTIVITY_LIST = [
   },
   {
     id: 11,
-    name: 'OIBC(5인)',
+    name: 'OIBC 빅데이터 챌린지(5인)',
     category: CATEGORIES.PROJECT,
     imageUrl: '/images/thumbnail/img-thumbnail-11.png',
-    youtubeVideoId: '-MmY3UvXXZE',
+    youtubeVideoId: null,
     downloadLinkUrl: null,
     webpageLinkUrl: null,
     githubUrls: {
-      gameUrl: 'https://github.com/Danto7632/LeafTale'
+      clientUrl: null
     },
-    summary: 'KES 2024 (동양미래EXPO)',
+    summary: '전기가격 예측을 위한 AI모델링',
     introduction:
-      '🚀 "프로젝트의 결정판!"\nEXPO 프로젝트는 한 해 동안의 노력을 결실로 맺는 자리입니다.',
+      '🧠 "OIBC Challenge"는 데이터로 전력 시장의 미래를 예측하는 도전의 장입니다! 창의적인 전략과 첨단 기술을 활용해 제주 전력 시장의 가격 변동성을 예측하며, 데이터로 혁신을 이끄는 흥미진진한 여정에 함께하세요!',
     description:
-      '🏆 EXPO 프로젝트는 동아리의 대표 작품을 선보이는 중요한 행사입니다. 각 팀은 자신의 프로젝트를 발표하고, 그 성과를 평가받습니다.\n\n' +
-      '💻 프로젝트 내용: 웹 애플리케이션, 게임 개발, 인공지능 등.\n\n' +
-      '🎉 2024년도 EXPO 선정심사에 합격하게 되어 추가적인 개발후에 10월 중 코엑스에서 KES2024 전시가 이뤄집니다.',
+    '🏢 OIBC 빅데이터 챌린지는 H Energy, POSTECH 오픈이노베이션 빅데이터 센터(OIBC), 산업 인공지능 프로그램이 공동 주최하는 빅데이터 경진대회입니다.\n\n'+
+    '💡 참가자들은 제주 전력 시장의 전기가격 예측 알고리즘을 개발하며, 데이터 기반의 혁신적 솔루션을 제시해야 합니다.\n\n'+
+    '🎮 주요 활동: 데이터 분석, 전력 시장 예측 모델 개발, API 연동, 결과 시각화, 산출값 보정\n\n'+
+    '🚀 api 활용과 데이터분석, AI모델링을 경험해보기 위해 참여하였고, 200이 넘는 팀 중 최종 18위로 마감하였습니다.(17위까지 입상)',
       activityImages: [
-        '/images/thumbnail/img-thumbnail-9.png',
-        '/images/activity/img-expo-1.png',
-        '/images/activity/img-expo-2.png',
-        '/images/activity/img-expo-3.png',
-        '/images/activity/img-expo-4.png',
-        '/images/activity/img-expo-5.png',
+        '/images/activity/img-oibc-1.png',
+        '/images/activity/img-oibc-2.png',
+        '/images/activity/img-oibc-3.png',
+        '/images/activity/img-oibc-4.png',
+        '/images/activity/img-oibc-5.png',
+        '/images/activity/img-oibc-6.png',
+        '/images/activity/img-oibc-7.png',
+        '/images/activity/img-oibc-8.png',
+        '/images/activity/img-oibc-9.png',
+        '/images/activity/img-oibc-10.png',
+        '/images/activity/img-oibc-11.png',
+        '/images/activity/img-oibc-12.png'
       ]
   },
   {
     id: 12,
-    name: '동양미래 EXPO 최종',
+    name: 'EXPO 프로젝트(14인) 최종결과 - 대상',
     category: CATEGORIES.PROJECT,
     imageUrl: '/images/thumbnail/img-thumbnail-12.png',
-    youtubeVideoId: 'w6NejB-W_To',
+    youtubeVideoId: 'Lk9xxMedmyE',
     downloadLinkUrl: null,
     webpageLinkUrl: null,
     githubUrls: {
       clientUrl: null,
       serverUrl: null,
-      gameUrl: 'https://github.com/Danto7632/GDP',
+      gameUrl: 'https://github.com/Danto7632/leaf_tale?tab=readme-ov-file',
     },
-    summary: '최종결과',
+    summary: '컴퓨터비전 AI를 활용한 손 재활발달 플랫폼',
     introduction:
-      '🐍 "GDP"는 제가 개발에 참여한 또 다른 프로젝트입니다. 다양한 기술을 활용해 완성된 작품을 만나보세요!',
+      '📋 손 재활 기기는 고가이며, 개개인의 손 크기와 발달 상태에 맞춘 최적화가 어려워 이를 해결하고자 컴퓨터 비전 AI 센서인 립모션(Leap Motion) 을 활용하여 손의 움직임을 실시간으로 분석하고 손 기능 개선과 발달을 돕는 게임 기반 손 재활 및 발달 플랫폼을 개발하였습니다.',
     description:
-      '🧩 GDP는 Unity와 C#으로 다양한 알고리즘을 활용해 완성된 창의적인 프로젝트입니다. Ex) 원형큐를 이용하여 몹 스폰 알고리즘을 구현하였습니다.\n\n' +
-      '🎮 주요 개발: 게임기능구현, 캐릭터 & 몹 & 배경디자인 (영상 속에 나오는 모든 것)\n\n' +
-      '🌟 GDP는 G(게임) D(동계) P(프로젝트)의 약자로 게임을 흥미를 가지게 된 동아리 부원끼리 단기로 만들어본 작품입니다!',
+      '🎮 손과 손목의 가동 범위 개선, 민첩성 훈련, 유연성 향상을 목표로 다양한 게임을 제공합니다.\n\n'+
+      '📊 게임 중 수집된 데이터를 바탕으로 재활의 진행 상황을 데이터 분석 기반 대시보드로 시각화합니다.\n\n'+
+      '💰 기존 재활 장비와 달리 저렴한 비용으로 누구나 쉽게 접근할 수 있는 솔루션입니다.\n\n\n' +
+
+      '💡 *기대효과 및 사업화 가능성*\n\n'+
+      '🏥 의료 기관과 협력해 재활 치료 비용과 시간을 절감하는 데 기여할 수 있습니다.\n\n'+
+      '🏠 손 기능 재활을 대중화해 일반 소비자 대상 B2C 솔루션으로 성장할 가능성이 큽니다.\n\n'+
+      '📜 현재 특허 출원 가능성을 검토 중이며, 상용화 가능성에 대한 긍정적인 평가를 기대합니다.\n\n\n'+
+
+      '🏆 최종적으로 동양미래EXPO 대상과 산학연협력EXPO 우수상을 수상하게 되었습니다.',
       activityImages: [
-        '/images/activity/img-gdp-1.png',
-        '/images/activity/img-gdp-2.png',
-        '/images/activity/img-gdp-3.png',
-        '/images/activity/img-gdp-4.png',
-        '/images/activity/img-gdp-5.png',
-        '/images/activity/img-gdp-6.png'
+        '/images/activity/img-expo2-1.png',
+        '/images/activity/img-expo2-2.png',
+        '/images/activity/img-expo2-7.png',
+        '/images/activity/img-expo2-8.png',
+        '/images/activity/img-expo2-9.jpeg',
+        '/images/activity/img-expo2-10.jpeg',
+        '/images/activity/img-expo2-11.png',
+        '/images/activity/img-expo2-12.png'
       ]
   }
 
